674e0d3385066be0f0ef286e38a15775ff252430
Ethan Leung <ethan@chartiq.com>
2021-09-09 12:03:53 +0800

    Use native-base

--- App.js
+++ App.js
@@ -6,43 +6,102 @@
  * @flow strict-local
  */
 
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import HUD from './SampleComponents/HUD';
-
 import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
+  VStack,
+  HStack,
+  Button,
+  IconButton,
+  Icon,
   Text,
+  NativeBaseProvider,
+  Center,
+  Box,
   StatusBar,
-} from 'react-native';
+} from 'native-base';
 
-import {
-  Header,
-  Colors,
-} from 'react-native/Libraries/NewAppScreen';
+import {SafeAreaView, StyleSheet, ScrollView, View} from 'react-native';
 
-import { WebView } from 'react-native-webview';
+import {Header, Colors} from 'react-native/Libraries/NewAppScreen';
+
+import {WebView} from 'react-native-webview';
+
+import {MaterialIcons} from '@expo/vector-icons';
+
 // ...
-const App: () => React$Node = () => {
+const App = () => {
   const [price, setPrice] = useState(0);
-  const [symbol, setSymbol] = useState(' ');
+  const [symbol, setSymbol] = useState('AAPL');
 
   return (
     <>
-      <Header />
-      <HUD value={price} symbol={symbol} />
-      <WebView
-        source={{ uri: 'http://localhost:3000/sample-template-native-sdk.html' }}
-        onMessage={event => {
-          const { data } = event.nativeEvent
-          const parsedData = JSON.parse(data)
-          const dataSet = parsedData.dataSet
-          setPrice(dataSet[dataSet.length - 1].Close)
-          setSymbol(parsedData.symbol)
-        }}
-      />
+      {/* <Header />
+      <HUD value={price} symbol={symbol} /> */}
+      <NativeBaseProvider>
+        <AppBar />
+        <WebView
+          source={{
+            uri: 'http://localhost:3000/sample-template-native-sdk.html',
+          }}
+          onMessage={(event) => {
+            const {data} = event.nativeEvent;
+            const parsedData = JSON.parse(data);
+            const dataSet = parsedData.dataSet;
+            setPrice(dataSet[dataSet.length - 1].Close);
+            setSymbol(parsedData.symbol);
+          }}
+        />
+      </NativeBaseProvider>
+    </>
+  );
+};
+
+const AppBar = () => {
+  return (
+    <>
+      <StatusBar backgroundColor="#000000" barStyle="light-content" />
+
+      <Box safeAreaTop backgroundColor="#000000" />
+
+      <HStack
+        bg="#000000"
+        px={1}
+        py={3}
+        justifyContent="space-between"
+        alignItems="center">
+        <HStack space={4} alignItems="center">
+          <IconButton
+            icon={
+              <Icon
+                size="sm"
+                as={<MaterialIcons name="chevron-left" />}
+                color="white"
+              />
+            }
+          />
+        </HStack>
+        <HStack space={2}>
+          <IconButton
+            icon={
+              <Icon
+                as={<MaterialIcons name="favorite" />}
+                size="sm"
+                color="white"
+              />
+            }
+          />
+          <IconButton
+            icon={
+              <Icon
+                as={<MaterialIcons name="search" />}
+                color="white"
+                size="sm"
+              />
+            }
+          />
+        </HStack>
+      </HStack>
     </>
   );
 };
